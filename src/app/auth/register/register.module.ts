b4ec5005d55e9c1d3d50142e077a4f2141bd9137
lidia_strelets <lidia_strelets@MacBook-Air-lidia-strelets.local>
2022-10-06 15:49:02 +0200

    add header component

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -8,6 +8,7 @@ import { RegisterPageRoutingModule } from './register-routing.module';
 
 import { RegisterPage } from './register.page';
 import { ErrorsComponent } from './errors/errors.component';
+import { HeaderComponent } from '../header/header.component';
 
 @NgModule({
   imports: [
@@ -16,6 +17,6 @@ import { ErrorsComponent } from './errors/errors.component';
     IonicModule,
     RegisterPageRoutingModule,
   ],
-  declarations: [RegisterPage, ErrorsComponent],
+  declarations: [RegisterPage, ErrorsComponent, HeaderComponent],
 })
 export class RegisterPageModule {}
