c9db12e20436e5bf361eb090044186800a55b34e
lidia_strelets <lidia_strelets@MacBook-Air-lidia-strelets.local>
2022-11-13 14:13:02 +0100

    move services to separate folder

--- src/app/components/home/services/hall.service.ts
+++ src/app/components/home/services/hall.service.ts
@@ -4,15 +4,22 @@ import { catchError, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Hall } from 'src/types';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HallService {
   url = `${environment.basicUrl}halls`;
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) {}
 
-  get(): Observable<Hall[]> {
+  get(): Observable<Hall[]> | null {
+    if (!this.authService.getCurrentUserId()) {
+      return null;
+    }
     return this.httpClient.get<Hall[]>(this.url).pipe(
       catchError((err) => {
         throw err;
