bbc2229f9abb9069caa58b30b7f689dc615af376
lidia_strelets <lidia_strelets@MacBook-Air-lidia-strelets.local>
2022-12-19 21:15:57 +0100

    disable change detection on scroll

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -4,3 +4,4 @@
  */
 // eslint-disable-next-line no-underscore-dangle
 (window as any).__Zone_disable_customElements = true;
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'ionScroll'];
